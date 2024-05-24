fa9aa9cd8c50683c41956b09414827c5eb6b29b2
Patrick Seo <pseo24@students.polytechnic.org>
2024-05-24 02:04:21 -0700

    Fixed BinaryTree and HashTable (hopefully)

--- atds.py
+++ atds.py
@@ -266,311 +266,47 @@ class UnorderedListStack(object):
         return str(self.ul)
 
 
-def main():
-    print("Testing the UnorderedList class")
-    tests_passed = 0
-    try:
-        ul = UnorderedList()
-        tests_passed += 1
-        print("Test passed: UnorderedList object created")
-    except:
-        print("Test failed: couldn't create UnorderedList object")
-
-    try:
-        if ul.is_empty():
-            tests_passed += 1
-            print("Test passed: .is_empty() method detected empty list")
-        else:
-            print("Test failed: .is_empty() method didn't understand that list is empty")
-    except:
-        print("Test failed: .is_empty method unavailable")
-    
-    try:
-        if ul.length() == 0:
-            tests_passed += 1
-            print("Test passed: .length() correctly identified a length of 0")
-        else:
-            print("Test failed: .length() didn't identify a length of 0")
-    except:
-        print("Test failed: .length() method unavailable")
-        
-    try:
-        ul.add(4)
-        ul.add(3)
-        ul.add(2)
-        ul.add(1)
-        ul.add(0)
-        tests_passed += 1
-        print("Test passed: five items added")
-    except:
-        print("Test failed: couldn't add items")
-    
-    try:
-        if not ul.is_empty():
-            tests_passed += 1
-            print("Test passed: .is_empty() method identified that list is no longer empty")
-        else:
-            print("Test failed: .is_empty() method thought list was empty, and it isn't")
-    except:
-        print("Test failed: .is_empty method unavailable")
-    
-    try:
-        if ul.length() == 5:
-            tests_passed += 1
-            print("Test passed: .length() correctly identified a length of 5")
-        else:
-            print("Test failed: .length() didn't identify a length of 5")
-    except:
-        print("Test failed: .length() method unusable")
-     
-    try:
-        result = str(ul)         # Try using __repr__
-        if result == "UnorderedList[0,1,2,3,4,]":
-            tests_passed += 1
-            print("Test passed: __repr__ returning correct values:")
-            print(result)
-        else:
-            print("Test failed: __repr__ returned " + result)
-            print("Expected: UnorderedList[0,1,2,3,4,]")
-        
-    except:
-        print("Test failed: couldn't reference __repr__ method")
-    
-    try:
-        if not ul.search(5):
-            tests_passed += 1
-            print("Test passed: .search() method correctly identified that 5 isn't on list")
-        else:
-            print("Test failed: .search() method thought 5 is on list when it isn't")
-    except:
-        print("Test failed: .search() method unavailable")
-        
-    try:
-        if ul.search(3):
-            tests_passed += 1
-            print("Test passed: .search() method correctly identified that 3 is on list")
-        else:
-            print("Test failed: .search() method thought 3 is on list when it isn't")
-    except:
-        print("Test failed: .search() method unavailable")
-    
-    try:
-        ul.remove(0)
-        if str(ul) == "UnorderedList[1,2,3,4,]":
-            tests_passed += 1
-            print("Test passed: .remove() successfully removed item from beginning of list")
-        else:
-            print("Test failed: .remove() didn't remove item from beginning of list")
-    except:
-        print("Test failed: .remove() method unavailable or not working?")
-
-    try:
-        ul = UnorderedList()
-        ul.add(2)
-        ul.add(1)
-        ul.add(0)
-        ul.remove(1)
-        if str(ul) == "UnorderedList[0,2,]":
-            tests_passed += 1
-            print("Test passed: .remove() successfully removed item from middle of list")
-        else:
-            print("Test failed: .remove() didn't remove item from middle of list")
-    except:
-        print("Test failed: .remove() method unavailable or not working?")
-        
-    try:
-        ul = UnorderedList()
-        ul.add(2)
-        ul.add(1)
-        ul.add(0)
-        ul.remove(2)
-        if str(ul) == "UnorderedList[0,1,]":
-            tests_passed += 1
-            print("Test passed: .remove() successfully removed item from end of list")
-        else:
-            print("Test failed: .remove() didn't remove item from end of list")
-    except:
-        print("Test failed: .remove() method unavailable or not working?")
-    
-    try:
-        ul = UnorderedList()
-        ul.add(2)
-        ul.add(1)
-        ul.add(0)
-        ul.remove(3)
-        if str(ul) == "UnorderedList[0,1,2,]":
-            tests_passed += 1
-            print("Test passed: .remove() successfully didn't remove item from list")
-        else:
-            print("Test failed: .remove() failed in not removing a non-existent item from list")
-    except:
-        print("Test failed: .remove() method unavailable or not working?")
-    
-    try:
-        ul = UnorderedList()
-        ul.add(0)
-        ul.add(1)
-        ul.add(0)
-        print("Attempting multiple remove")
-        print("Before remove: " + str(ul))
-        ul.remove(0)
-        print("After remove: " + str(ul))
-        result = ul.search(0)
-        if not result:
-            tests_passed += 1
-            print("Test passed: .remove() successfully removed multiple items")
-        else:
-            print("Test failed: .remove() didn't remove multiple items")
-    except:
-        print("Test failed: .remove() method unavailable or not working?")
-    
-    try:
-        ul = UnorderedList()
-        ul.add(2)
-        ul.add(1)
-        ul.add(0)
-        ul.append(3)
-        if str(ul) == "UnorderedList[0,1,2,3,]":
-            tests_passed += 1
-            print("Test passed: .append() successfully appended item to list")
-        else:
-            print("Test failed: .append() didn't append item to list")
-    except:
-        print("Test failed: .append() method unavailable or not working?")
-    
-    try:
-        result = ul.index(1)
-        if result == 1:
-            tests_passed += 1
-            print("Test passed: .index() successfully found item on list")
-        else:
-            print("Test failed: .index() failed to find item on list")
-    except:
-        print("Test failed: .index() method unavailable? (or __repr__ not working?)")
-    
-    try:
-        ul = UnorderedList()
-        ul.add(2)
-        ul.add(1)
-        ul.add(0)
-        ul.insert(0, -1)
-        if str(ul) == "UnorderedList[-1,0,1,2,]":
-            tests_passed += 1
-            print("Test passed: .insert() successfully inserted value at beginning of list")
-        else:
-            print("Test failed: .insert() didn't correctly insert at beginning of list")
-    except:
-        print("Test failed: .insert() method unavailable? (or __repr__ not working?)")
-    
-    try:
-        ul = UnorderedList()
-        ul.add(2)
-        ul.add(1)
-        ul.add(0)
-        ul.insert(2,3)
-        if str(ul) == "UnorderedList[0,1,3,2,]":
-            tests_passed += 1
-            print("Test passed: .insert() successfully inserted values in middle of list")
-        else:
-            print("Test failed: .insert() didn't correctly insert values in middle of list")
-    except:
-        print("Test failed: .insert() method unavailable? (or __repr__ not working?)")
-    
-    try:
-        ul = UnorderedList()
-        ul.add(2)
-        ul.add(1)
-        ul.add(0)
-        ul.pop()
-        if str(ul) == "UnorderedList[0,1,]":
-            tests_passed += 1
-            print("Test passed: .pop() successfully removed last item from list")
-        else:
-            print("Test failed: .pop() didn't remove last item from list correctly")
-    except:
-        print("Test failed: .pop() method unavailable? (or __repr__ not working?)")
-    
-    try:
-        ul = UnorderedList()
-        ul.add(2)
-        ul.add(1)
-        ul.add(0)
-        ul.pop(0)
-        if str(ul) == "UnorderedList[1,2,]":
-            tests_passed += 1
-            print("Test passed: .pop(0) successfully removed first item from list")
-        else:
-            print("Test failed: .pop(0) didn't remove first item from list correctly")
-    except:
-        print("Test failed: .pop(0) method unavailable? (or __repr__ not working?)")
-
-    try:
-        ul = UnorderedList()
-        ul.add(2)
-        ul.add(1)
-        ul.add(0)
-        ul.pop(1)
-        if str(ul) == "UnorderedList[0,2,]":
-            tests_passed += 1
-            print("Test passed: .pop(1) successfully removed item from middle of list")
-        else:
-            print("Test failed: .pop(1) didn't remove last item from middle of list correctly")
-    except:
-        print("Test failed: .pop(1) method unavailable? (or __repr__ not working?)")
-
-
-        
-    print(str(tests_passed) + "/21 tests passed on the UnorderedList class")
-
 class HashTable(object):
-    """Describes a hash table based on two lists, 'slots' and 'values', and describes putting and getting values onto that table.
-    Hash function is the mod (%) function, and collisions are handled using linear probing.
-    """
     def __init__(self, size):
-        """Create empty lists for the Map
-        """
         self.keys = size * [None]
         self.data = size * [None]
         self.size = size
 
     def put(self, key, value):
-        """Creates an entry in the hash table
-        """
-        hash_value = key % self.size            # index for key & value
+        
+        hash_value = key % self.size
         while self.keys[hash_value] != None and self.keys[hash_value] != key:
             hash_value += 1
-        # We're at a position where we can place the value
-        if self.keys[hash_value] == key: 
+        if self.keys[hash_value] == key:
             self.data[hash_value] = value
         else:
-            # begin the linear probe
             self.keys[hash_value] = key
             self.data[hash_value] = value
-    
+        
     def get(self, key):
         hash_value = key % self.size
         while self.keys[hash_value] != None and self.keys[hash_value] != key:
             hash_value += 1
-        if self.keys[hash_value] == key: 
+        if self.keys[hash_value] == key:
             return self.data[hash_value]
         else:
             return None
-    
+        
     def __str__(self):
         return "Keys:   " + str(self.keys) + "\n" + \
                "Values: " + str(self.data)
-
-class BinaryTree:
-    def init(self, val):
+ 
+class BinaryTree(object):
+    def __init__(self, val):
         self.val = val
-        self.left_child  = None
+        self.left_child = None
         self.right_child = None
-
+        
     def get_root_val(self):
         return self.val
-
-    def set_root_val(self, new_val):
-        self.val = new_val
+    
+    def set_root_val(self, val):
+        self.val = val
 
     def get_left_child(self):
         return self.left_child
@@ -582,17 +318,17 @@ class BinaryTree:
         new_subtree = BinaryTree(val)
         new_subtree.left_child = self.left_child
         self.left_child = new_subtree
-
+    
     def insert_right(self, val):
         new_subtree = BinaryTree(val)
         new_subtree.right_child = self.right_child
         self.right_child = new_subtree
 
-    def repr(self):
-        return "BinaryTree[key = " + str(self.val) + \
-        ",left=" + str(self.left_child) + \
-        ", right=" + str(self.right_child) + "]"
-
+    def __str__(self):
+        return "BinaryTree[key=" + str(self.val) + \
+               ",left_child=" + str(self.left_child) + \
+               ",right_child=" + str(self.right_child) + "]"
+    
 def main():
     tests_passed = 0
     print("\nTEST: Creating HashTable(11)...")
@@ -624,8 +360,6 @@ def main():
         tests_passed += 1
     except:
         print("FAIL. Couldn't print using __str__ or __repr__")
-        
-
 
     print("\nTEST: Looking for original hash in table..")
     try:
